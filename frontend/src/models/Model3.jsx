c65ecd2dedab0d3d7eab792a4a1447ae7b69abcd
RohanBera <berarohan88@gmail.com>
2023-12-06 05:29:22 -0500

    Models with dynamic form text

--- frontend/src/models/Model3.jsx
+++ frontend/src/models/Model3.jsx
@@ -1,9 +1,106 @@
 import React from 'react'
+import axios from "axios";
+import { useState } from "react";
+import { model_fields } from './model_fields.js'
+import Tooltip from '@mui/material/Tooltip';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
 const Model3 = () => {
+
+    // TODO: change URL
+    const url = "http://localhost:5000/predict";
+    const [data, setData] = useState({
+        area: "",
+        victage: "",
+        victsex: "",
+        weaponused: "",
+    })
+    const [result, setResult] = useState('')
+
+    function submit(e) {
+        e.preventDefault();
+        try {
+            axios
+                .post(url, {
+                    area: data.area,
+                    victage: data.victage,
+                    victsex: data.victsex,
+                    weaponused: data.weaponused,
+                })
+                .then((res) => {
+                    console.log(res.data)
+                    setResult(res.data.prediction)
+                })
+        } catch (e) {
+            console.log('Error in API call', e);
+        }
+
+    }
+    function handle(e) {
+        const newdata = { ...data }
+        newdata[e.target.id] = e.target.value
+        setData(newdata)
+        setResult('')
+        console.log(newdata)
+    }
+    const model_info = JSON.parse(JSON.stringify(model_fields['model3']))
+
     return (
-        <div>Model3</div>
-    )
+
+        <div>
+            <h1 className='main-heading'>{model_info.name}</h1>
+
+            <h2 className="model-desc">{model_info.description}</h2>
+
+            <form className='form model lr' onSubmit={(e) => submit(e)}>
+                {
+                    model_info.fields.map((field) => {
+                        return (
+                            <label>
+                                <div className="label-text">
+
+                                    <p>{field.label}</p>
+                                    <Tooltip title={field.field_desc} placement="right">
+                                        <InfoOutlinedIcon />
+                                    </Tooltip>
+                                </div>
+                                {field.type !== 'dropdown' &&
+                                    <input
+                                        id={field.id}
+                                        placeholder={field.label}
+                                        type={field.type}
+                                        onChange={(e) => handle(e)}
+                                        required
+                                    ></input>
+                                }
+                                {field.type === 'dropdown' &&
+                                    <select
+                                        id={field.id}
+                                        disabled={!field.options.length}
+                                        placeholder='rohan'
+                                        required
+                                        // value={breed}
+                                        onChange={(e) => handle(e)}
+                                    >
+                                        <option value="" />
+                                        {field.options.map(option => (
+                                            <option value={option[1]}>{option[0]}</option>
+                                        ))}
+                                    </select>
+                                }
+                            </label>
+                        )
+
+                    })
+                }
+
+                <button className='submit' >Submit</button>
+            </form>
+
+            <strong>{result}</strong>
+        </div>
+    );
 }
+
 
 export default Model3
---
+++ frontend/src/models/model_fields.js
@@ -0,0 +1,159 @@
+const model_fields = {
+    "model1": {
+        "name": "Logistic Regression",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis neque non hendrerit pellentesque. Sed maximus, orci id pharetra rhoncus, enim tortor dictum leo, vel lacinia arcu purus sit amet nisi. In imperdiet, lorem non vulputate faucibus, lectus felis congue urna, vitae vulputate mi justo convallis nisi. Sed consequat tempus fringilla. Nam vehicula metus non hendrerit ornare. Etiam aliquet vel nisl at venenatis. Aenean condimentum volutpat lobortis. In sit amet ante et lorem placerat hendrerit ut eget urna. Aliquam ligula risus, pretium ac dolor id, fringilla auctor mi. Nam id lacus est. Praesent auctor gravida enim. Curabitur aliquet in lectus vitae bibendum. Nam vehicula odio vitae quam vestibulum, eu pulvinar erat tincidunt. Cras volutpat fermentum ultrices.",
+        "fields": [
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "area",
+                "label": "Area"
+            },
+            {
+                "type": "number",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "victage",
+                "label": "Victim Age"
+            },
+            {
+                "type": "dropdown",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "victsex",
+                "label": "Victim Sex",
+                "options": [
+                    ["male", 1],
+                    ["female", 0]
+                ]
+
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "weaponused",
+                "label": "Weapon Used"
+            }
+        ]
+    },
+    "model2": {
+        "name": "K Nearest Neighbours",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis neque non hendrerit pellentesque. Sed maximus, orci id pharetra rhoncus, enim tortor dictum leo, vel lacinia arcu purus sit amet nisi. In imperdiet, lorem non vulputate faucibus, lectus felis congue urna, vitae vulputate mi justo convallis nisi. Sed consequat tempus fringilla. Nam vehicula metus non hendrerit ornare. Etiam aliquet vel nisl at venenatis. Aenean condimentum volutpat lobortis. In sit amet ante et lorem placerat hendrerit ut eget urna. Aliquam ligula risus, pretium ac dolor id, fringilla auctor mi. Nam id lacus est. Praesent auctor gravida enim. Curabitur aliquet in lectus vitae bibendum. Nam vehicula odio vitae quam vestibulum, eu pulvinar erat tincidunt. Cras volutpat fermentum ultrices.",
+        "fields": [
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            }
+        ]
+    },
+    "model3": {
+        "name": "Decision Tree Classifier",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis neque non hendrerit pellentesque. Sed maximus, orci id pharetra rhoncus, enim tortor dictum leo, vel lacinia arcu purus sit amet nisi. In imperdiet, lorem non vulputate faucibus, lectus felis congue urna, vitae vulputate mi justo convallis nisi. Sed consequat tempus fringilla. Nam vehicula metus non hendrerit ornare. Etiam aliquet vel nisl at venenatis. Aenean condimentum volutpat lobortis. In sit amet ante et lorem placerat hendrerit ut eget urna. Aliquam ligula risus, pretium ac dolor id, fringilla auctor mi. Nam id lacus est. Praesent auctor gravida enim. Curabitur aliquet in lectus vitae bibendum. Nam vehicula odio vitae quam vestibulum, eu pulvinar erat tincidunt. Cras volutpat fermentum ultrices.",
+        "fields": [
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            }
+        ]
+    },
+    "model4": {
+        "name": "Model 4",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis neque non hendrerit pellentesque. Sed maximus, orci id pharetra rhoncus, enim tortor dictum leo, vel lacinia arcu purus sit amet nisi. In imperdiet, lorem non vulputate faucibus, lectus felis congue urna, vitae vulputate mi justo convallis nisi. Sed consequat tempus fringilla. Nam vehicula metus non hendrerit ornare. Etiam aliquet vel nisl at venenatis. Aenean condimentum volutpat lobortis. In sit amet ante et lorem placerat hendrerit ut eget urna. Aliquam ligula risus, pretium ac dolor id, fringilla auctor mi. Nam id lacus est. Praesent auctor gravida enim. Curabitur aliquet in lectus vitae bibendum. Nam vehicula odio vitae quam vestibulum, eu pulvinar erat tincidunt. Cras volutpat fermentum ultrices.",
+        "fields": [
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            }
+        ]
+    },
+    "model5": {
+        "name": "Model 5",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis neque non hendrerit pellentesque. Sed maximus, orci id pharetra rhoncus, enim tortor dictum leo, vel lacinia arcu purus sit amet nisi. In imperdiet, lorem non vulputate faucibus, lectus felis congue urna, vitae vulputate mi justo convallis nisi. Sed consequat tempus fringilla. Nam vehicula metus non hendrerit ornare. Etiam aliquet vel nisl at venenatis. Aenean condimentum volutpat lobortis. In sit amet ante et lorem placerat hendrerit ut eget urna. Aliquam ligula risus, pretium ac dolor id, fringilla auctor mi. Nam id lacus est. Praesent auctor gravida enim. Curabitur aliquet in lectus vitae bibendum. Nam vehicula odio vitae quam vestibulum, eu pulvinar erat tincidunt. Cras volutpat fermentum ultrices.",
+        "fields": [
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            },
+            {
+                "type": "text",
+                "field_desc": "Vestibulum id metus nec odio finibus lacinia vel vitae neque. Nam quis augue pellentesque, varius nulla vel, sodales purus. Praesent rutrum odio eget velit pretium tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pulvinar nibh ut metus lacinia sollicitudin. ",
+                "id": "",
+                "label": ""
+            }
+        ]
+    }
+}
+
+export { model_fields }
