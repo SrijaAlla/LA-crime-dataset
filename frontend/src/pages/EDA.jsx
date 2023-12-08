3cec3af47b1c7495e038152309fa756304c9f155
RohanBera <berarohan88@gmail.com>
2023-12-07 20:33:09 -0500

    Added bubble map to EDA

--- frontend/src/pages/EDA.jsx
+++ frontend/src/pages/EDA.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+// var bubble_chart = require('../bubble_chart_with_map.html');
+
+
 function EDA() {
     return (
-        <div>EDA</div>
+        <div className='eda'>
+
+            EDA
+
+
+            <div className="bubble-map">
+
+                <iframe title='Bubble Chart' src='bubble_chart_with_map.html' ></iframe>
+            </div>
+
+        </div>
     )
 }
 
